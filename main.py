8753a5943812c823d493dea20d2720e7531bfa93
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2024-07-04 20:33:19 +0100

    connect to ollama models

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from pytube import YouTube
 import speech_recognition as sr
 from pydub import AudioSegment
 from pydub.utils import make_chunks
+import ollama
 import os
+import shutil
+
+TEMP_FOLDER = "temp"
 
 # url = input("Enter the YouTube video URL: ")
 URL = "https://www.youtube.com/watch?v=rrB13utjYV4"
-TEMP_FOLDER = "temp"
-os.makedirs(TEMP_FOLDER, exist_ok=True)
 
 
 def get_youtube_audio(url):
@@ -49,12 +51,66 @@ def transcribe_audio(audio):
     return transcript
 
 
+def summarize(transcript, model):
+    response = ollama.chat(model=model, messages=[
+        {
+            'role': 'system',
+            'content': 'Your goal is to summarize the text given to you in not more than 400 words. It is a transcript of a youtube video. Ensure that you get all the major points focused on in the video.'
+        },
+        {
+            'role': 'user',
+            'content': transcript,
+        }
+    ])
+
+    return response['message']['content']
+
+
+### For connecting to the Ollama models, you can also directly use the request module ###
+# import requests
+
+# OLLAMA_ENDPOINT = "http://localhost:11434/api/generate"
+
+
+# def summarize(transcript, model):
+#     system_prompt = 'Your goal is to summarize the text given to you in not more than 400 words. It is a transcript of a youtube video. Ensure that you get all the major points focused on in the video.'
+
+#     headers = {
+#         "Content-Type" : "application/json"
+#     }
+
+#     data = {
+#         "model" : model,
+#         "prompt" : system_prompt + '\n\n' + transcript,
+#         "stream" : False,
+#         "keep_alive": "5m"
+#     }
+
+#     response = requests.post(OLLAMA_ENDPOINT, headers=headers, json=data)
+#     response.raise_for_status()
+
+#     return response.json()["response"]
+
+
+def clear_temp():
+    if os.path.exists(TEMP_FOLDER):
+        shutil.rmtree(TEMP_FOLDER)
+        os.makedirs(TEMP_FOLDER)
+    else:
+        os.makedirs(TEMP_FOLDER)
+
+
 def main():
+    clear_temp()
+
     audio = get_youtube_audio(URL)
     transcript = transcribe_audio(audio)
-
-    print(transcript)
+    summary = summarize(transcript=transcript, model="phi3")
+    
+    clear_temp()
+    
+    print(summary)
 
 
 if __name__ == "__main__":
-    main()
+    main()  
