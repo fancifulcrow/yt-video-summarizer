e12c90481721a06678e862867ab654ddd6ec1743
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2024-07-04 18:11:38 +0100

    breaking audio into chunks

--- main.py
+++ main.py
@@ -2,38 +2,59 @@ from pytube import YouTube
 import speech_recognition as sr
 from pydub import AudioSegment
 from pydub.utils import make_chunks
+import os
 
 # url = input("Enter the YouTube video URL: ")
-url = "https://www.youtube.com/watch?v=rrB13utjYV4"
+URL = "https://www.youtube.com/watch?v=rrB13utjYV4"
+TEMP_FOLDER = "temp"
+os.makedirs(TEMP_FOLDER, exist_ok=True)
 
-yt = YouTube(url)
 
-audio_stream = yt.streams.filter(only_audio=True).first()
-audio_stream.download(output_path='.', filename='sound.mp4')
+def get_youtube_audio(url):
+    try:
+        yt = YouTube(url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        filename = 'sound.mp4'
+        audio_stream.download(output_path=f"{TEMP_FOLDER}", filename=filename)
+        audio = AudioSegment.from_file(f"{TEMP_FOLDER}/{filename}")
 
-# Convert the audio to WAV format using pydub
-audio = AudioSegment.from_file("sound.mp4")
-
-r = sr.Recognizer()
-
-# Break the audio into chunks of 1 minute each (60000 ms)
-chunk_length_ms = 60000
-chunks = make_chunks(audio, chunk_length_ms)
-
-full_result = ""
-
-for i, chunk in enumerate(chunks):
-    chunk_filename = f"chunk{i}.wav"
-    chunk.export(chunk_filename, format="wav")
+        return audio
     
-    with sr.AudioFile(chunk_filename) as source:
-        audio_data = r.record(source)
-        try:
-            result = r.recognize_google(audio_data)
-            full_result += result + " "
-        except sr.RequestError as e:
-            print(f"Could not request results from Google Speech Recognition service; {e}")
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
-print(full_result)
+
+def transcribe_audio(audio):
+    r = sr.Recognizer()
+
+    # Break the audio into chunks of 1 minute each (60000 ms)
+    chunk_length_ms = 60000
+    chunks = make_chunks(audio, chunk_length_ms)
+
+    transcript = ""
+
+    for i, chunk in enumerate(chunks):
+        chunk_filename = f"{TEMP_FOLDER}/chunk{i}.wav"
+        chunk.export(chunk_filename, format="wav")
+        with sr.AudioFile(chunk_filename) as source:
+            audio_data = r.record(source)
+            try:
+                transcript += r.recognize_google(audio_data) + " "
+            except sr.RequestError as e:
+                print(f"Could not request results from Google Speech Recognition service; {e}")
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+        
+    return transcript
+
+
+def main():
+    audio = get_youtube_audio(URL)
+    transcript = transcribe_audio(audio)
+
+    print(transcript)
+
+
+if __name__ == "__main__":
+    main()
